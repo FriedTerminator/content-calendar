47d4868824e5ba157ded25b60ca3e37fc352d99d
FriedTerminator <nikita.cherepanov02@gmail.com>
2023-06-16 21:31:30 -0700

    creating repsitory

---
+++ src/main/java/dev/nikcherep/contentcalendar/MyWebConfig.java
@@ -0,0 +1,13 @@
+package dev.nikcherep.contentcalendar;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class MyWebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+}
